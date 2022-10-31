ddb37e00a790768eec6c077eb87d1ec76e454b79
spn <lailack913laley@gmail.com>
2022-10-31 23:12:03 +0300

    31/10/2022 ietemad

--- lib/5_saved/saved.dart
+++ lib/5_saved/saved.dart
@@ -21,48 +21,59 @@ class _savedState extends State<saved> {
         backgroundColor: Colors.grey,
       ),
       body:
-      Container(
-        width: MediaQuery.of(context).size.width/1.05,
-        child: ListView.builder(
-            itemCount: 1,
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            itemBuilder: (context, pos){
-              return
-                Center(
-                  child: Column(
-                    children: [
+      SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width/1.05,
+             alignment: Alignment.center,
+              child: ListView.builder(
+                  itemCount: 1,
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, pos){
+                    return
+                      Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
 
-                      post_templates.post_residential("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
-                          "مشتمل", "بيع", "بغداد", "الدورة", 150, 12, "مليون د.ع",
-                          1, 2, 3, 4,
-                          MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
+                            post_templates.post_residential("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
+                                "مشتمل", "بيع", "بغداد", "الدورة", 150, 12, "مليون د.ع",
+                                1, 2, 3, 4,
+                                MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
 
-                      SizedBox(height: MediaQuery.of(context).size.height/17,),
+                            SizedBox(height: MediaQuery.of(context).size.height/17,),
 
-                      post_templates.post_land("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
-                          "سكية", "بغداد", "العامرية", 300, 120, "مليون د.ع",
-                          MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
+                            post_templates.post_land("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
+                                "سكية", "بغداد", "العامرية", 300, 120, "مليون د.ع",
+                                MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
 
-                      SizedBox(height: MediaQuery.of(context).size.height/17,),
+                            SizedBox(height: MediaQuery.of(context).size.height/17,),
 
-                      post_templates.post_store("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
-                          "متجر احذية", "بيع", "البصرة", "شارع اجزائر", 100, 30, "مليون د.ع",
-                          MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
+                            post_templates.post_store("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
+                                "متجر احذية", "بيع", "البصرة", "شارع اجزائر", 100, 30, "مليون د.ع",
+                                MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
 
-                      SizedBox(height: MediaQuery.of(context).size.height/17,),
+                            SizedBox(height: MediaQuery.of(context).size.height/17,),
 
-                      post_templates.post_buildings("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
-                          "تجاري", "للبيع", "البصرة", "الجزائر", 370, 2.7,"مليار د.ع", 3,
-                          MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
+                            post_templates.post_buildings("https://media.architecturaldigest.com/photos/57c7003fdc03716f7c8289dd/master/pass/IMG%20Worlds%20of%20Adventure%20-%201.jpg",
+                                "تجاري", "للبيع", "البصرة", "الجزائر", 370, 2.7,"مليار د.ع", 3,
+                                MediaQuery.of(context).size.height/2.9, MediaQuery.of(context).size.height/5.3),
 
-                      SizedBox(height: MediaQuery.of(context).size.height/17,),
+                            SizedBox(height: MediaQuery.of(context).size.height/17,),
 
 
-                    ],
-                  ),
-                );
-            }),
+                          ],
+                        ),
+                      );
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
